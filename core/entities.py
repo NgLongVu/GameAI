24a5085a5d60aa03bd3f0574b381f7888d69ef63
Hieunq123 <huynq123123@gmail.com>
2026-04-14 21:38:59 +0700

    map1

---
+++ core/entities.py
@@ -0,0 +1,48 @@
+import pygame
+from utils.constants import LERP_SPEED, WINDOW_WIDTH, WINDOW_HEIGHT
+
+class Entity:
+    def __init__(self, start_node, color=(0,0,0)):
+        self.current_node = start_node
+        self.target_node = start_node
+        # Start exactly at the node's position
+        self.visual_x = float(start_node.x)
+        self.visual_y = float(start_node.y)
+        self.color = color
+        self.radius = 20
+
+    def move_to(self, node):
+        """Logic movement, visual movement will follow smoothly via update()"""
+        self.current_node = node
+        self.target_node = node
+
+    def is_animating(self):
+        dx = abs(self.visual_x - self.target_node.x)
+        dy = abs(self.visual_y - self.target_node.y)
+        return dx > 1.0 or dy > 1.0
+
+    def update(self):
+        """LERP to target node"""
+        self.visual_x += (self.target_node.x - self.visual_x) * LERP_SPEED
+        self.visual_y += (self.target_node.y - self.visual_y) * LERP_SPEED
+
+    def draw(self, screen):
+        # Temp shape logic, should load images ideally
+        pygame.draw.circle(screen, self.color, (int(self.visual_x), int(self.visual_y)), self.radius)
+        # Give border
+        pygame.draw.circle(screen, (0, 0, 0), (int(self.visual_x), int(self.visual_y)), self.radius, 2)
+
+class Police(Entity):
+    def __init__(self, start_node):
+        # Police represented by dark blue
+        super().__init__(start_node, color=(20, 40, 100))
+        self.radius = 22
+
+class Thief(Entity):
+    def __init__(self, start_node):
+        # Thief represented by dark grey
+        super().__init__(start_node, color=(80, 80, 80))
+        self.radius = 18
+
+    def is_at_exit(self):
+        return self.current_node.type == 'exit'
